a559fa087c3d815c4fda0bd86c362a7154129d61
tunjiit611 <131035697+tunjiit611@users.noreply.github.com>
2023-05-20 08:15:18 -0400

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "my-aws-tf-state-bucket"
+    bucket = "tunjiit611bucket"
     key = "main"
     region = "us-east-1"
-    dynamodb_table = "my-dynamo-db-table"
+    dynamodb_table = "tunjiit611table"
   }
 }
